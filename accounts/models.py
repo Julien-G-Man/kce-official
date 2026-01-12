98122407b62ed4c8ea7b7eaf7c6965ff3e2542ca
Julien-G-Man <juliengmanana@gmail.com>
2026-01-12 07:27:39 +0100

    Add superuser creation script

--- accounts/models.py
+++ accounts/models.py
@@ -5,4 +5,9 @@ from django.contrib.auth.models import AbstractUser
 # Option 2 (future): Create a custom user model by subclassing AbstractUser.
 
 class CustomUser(AbstractUser): 
-   pass
+   # CustomUser class in inheritinng from the AbstractUser class
+   # Add extra fields here when needed, e.g. role (admin/member/pastor)
+   role = models.CharField(max_length=50, default="member")
+
+   def __str__(self):
+      return self.username
